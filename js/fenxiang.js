dbee9a4f9ccd6bd3789f8ac6bf3d336bf8d884c1
黄小丫 <853007191@qq.com>
2018-04-08 19:41:18 +0800

    南潮嘉音

---
+++ js/fenxiang.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+
+
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"1","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"24"},"slide":{"type":"slide","bdImg":"6","bdPos":"left","bdTop":"53.5"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+
+
+
+
